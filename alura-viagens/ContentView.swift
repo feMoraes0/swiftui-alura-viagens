6306e3acc85dda6fccdac819ff8e6b0ea4dbdf9d
feMoraes0 <fernandomoraes.lopes@gmail.com>
2022-06-07 13:28:33 -0300

    Initial commit

---
+++ alura-viagens/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  alura-viagens
+//
+//  Created by Fernando Moraes on 07/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Primeira aula de SwiftUI")
+            .padding()
+    }
+}
+
+// Serve somente para visualização da preview
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ alura-viagens/alura_viagensApp.swift
@@ -0,0 +1,17 @@
+//
+//  alura_viagensApp.swift
+//  alura-viagens
+//
+//  Created by Fernando Moraes on 07/06/22.
+//
+
+import SwiftUI
+
+@main
+struct alura_viagensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
