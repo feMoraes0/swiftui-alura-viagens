c75640d6d1e5fdbf5848fe90c2808774b1db5250
feMoraes0 <fernandomoraes.lopes@gmail.com>
2022-06-08 18:13:57 -0300

    lesson: add and customizing buttons

--- alura-viagens/ContentView.swift
+++ alura-viagens/ContentView.swift
@@ -9,30 +9,60 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Text("alura viagens")
-                .foregroundColor(.white)
-                .font(.custom("Avenir Black", size: 20))
-            Text("ESPECIAL")
-                .foregroundColor(.white)
-                .font(.custom("Avenir Book", size: 20))
-                .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
-                .padding(.leading, 30)
-            Text("BRASIL")
-                .foregroundColor(.white)
-                .bold()
-                .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
-                .padding(.leading, 30)
-            
-            List {
-                Text("Rio de janeiro")
-                Text("Ceará")
-                Text("Atibaia")
-                Text("São Paulo")
+        GeometryReader { view in
+            VStack {
+                VStack {
+                    Text("alura viagens")
+                        .foregroundColor(.white)
+                        .font(.custom("Avenir Black", size: 20))
+                    Text("ESPECIAL")
+                        .foregroundColor(.white)
+                        .font(.custom("Avenir Book", size: 20))
+                        .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                        .padding(.leading, 30)
+                    Text("BRASIL")
+                        .foregroundColor(.white)
+                        .font(.custom("Avenir Black", size: 23))
+                        .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                        .padding(.leading, 30)
+                }
+                .frame(width: view.size.width, height: 180, alignment: .top)
+                .background(.purple)
+                
+                HStack {
+                    Button("Hotéis") {
+                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                    }.font(.custom("Avenir Medium", size: 17))
+                    .foregroundColor(.white)
+                    .frame(width: 100, height: 50)
+                    .overlay(RoundedRectangle(cornerRadius: 10).stroke(.blue, lineWidth: 10))
+                    .background(.blue)
+                    .offset(x: 50)
+                    
+                    Spacer()
+
+                    Button("Pacotes") {
+                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                    }
+                    .font(.custom("Avenir Medium", size: 17))
+                    .foregroundColor(.white)
+                    .frame(width: 100, height: 50)
+                    .overlay(RoundedRectangle(cornerRadius: 10).stroke(.orange, lineWidth: 10))
+                    .background(.orange)
+                    .offset(x: -50)
+                }
+                .offset(x: 0, y: -35)
+                
+                
+                List {
+                    Text("Rio de janeiro")
+                    Text("Ceará")
+                    Text("Atibaia")
+                    Text("São Paulo")
+                }
             }
             
         }
-        .background(.purple)
     }
 }
 
