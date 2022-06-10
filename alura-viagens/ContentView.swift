5de04a2c286fb6100213dd51d2557b338b657afd
feMoraes0 <fernandomoraes.lopes@gmail.com>
2022-06-10 17:51:24 -0300

    lesson: updates some components size based in an environment variable

--- alura-viagens/ContentView.swift
+++ alura-viagens/ContentView.swift
@@ -8,11 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.horizontalSizeClass) var horizontalSizeClass;
+    
+    func isHorizontalSizeCompact() -> Bool {
+        return self.horizontalSizeClass == .compact;
+    }
+    
     var body: some View {
         GeometryReader { view in
             VStack {
                 HeaderView()
-                    .frame(width: view.size.width, height: 200, alignment: .top)
+                    .frame(width: view.size.width, height: self.isHorizontalSizeCompact() ? 200 : 315, alignment: .top)
                 List(viagens) { viagem in
                     CelulaViagemView(viagem: viagem)
                 }
--- alura-viagens/Views/HeaderView.swift
+++ alura-viagens/Views/HeaderView.swift
@@ -21,6 +21,7 @@ struct HeaderView: View {
                     Text("alura viagens")
                         .foregroundColor(.white)
                         .font(.custom("Avenir Black", size: self.isHorizontalSizeCompact() ? 20 : 30))
+                        .padding(.top, self.isHorizontalSizeCompact() ? 0 : 80)
                     Text("ESPECIAL")
                         .foregroundColor(.white)
                         .font(.custom("Avenir Book", size: self.isHorizontalSizeCompact() ? 20 : 30))
@@ -32,32 +33,32 @@ struct HeaderView: View {
                         .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
                         .padding(.leading, 30)
                 }
-                .frame(width: view.size.width, height: 180, alignment: .top)
+                .frame(width: view.size.width, height: self.isHorizontalSizeCompact() ? 200 : 280, alignment: .top)
                 .background(.purple)
                 
                 HStack {
                     Button("Hotéis") {
                         /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
-                    }.font(.custom("Avenir Medium", size: 17))
+                    }.font(.custom("Avenir Medium", size: self.isHorizontalSizeCompact() ? 17 : 24))
                     .foregroundColor(.white)
-                    .frame(width: 100, height: 50)
+                    .frame(width: self.isHorizontalSizeCompact() ? 100 : 150, height: self.isHorizontalSizeCompact() ? 50 : 75)
                     .overlay(RoundedRectangle(cornerRadius: 10).stroke(.blue, lineWidth: 10))
                     .background(.blue)
-                    .offset(x: 50)
+                    .offset(x: self.isHorizontalSizeCompact() ? 50 : view.size.width / 4)
                     
                     Spacer()
 
                     Button("Pacotes") {
                         /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
                     }
-                    .font(.custom("Avenir Medium", size: 17))
+                    .font(.custom("Avenir Medium", size: self.isHorizontalSizeCompact() ? 17 : 24))
                     .foregroundColor(.white)
-                    .frame(width: 100, height: 50)
+                    .frame(width: self.isHorizontalSizeCompact() ? 100 : 150, height: self.isHorizontalSizeCompact() ? 50 : 75)
                     .overlay(RoundedRectangle(cornerRadius: 10).stroke(.orange, lineWidth: 10))
                     .background(.orange)
-                    .offset(x: -50)
+                    .offset(x: self.isHorizontalSizeCompact() ? -50 : -view.size.width / 4)
                 }
-                .offset(y: -35)
+                .offset(y: self.isHorizontalSizeCompact() ? -35 : -50)
             }
         }
     }
@@ -65,6 +66,14 @@ struct HeaderView: View {
 
 struct HeaderView_Previews: PreviewProvider {
     static var previews: some View {
-        HeaderView()
+        Group {
+            HeaderView()
+                .environment(\.horizontalSizeClass, .compact)
+                .previewLayout(.fixed(width: 400, height: 230))
+            HeaderView()
+                .environment(\.horizontalSizeClass, .regular)
+                .previewLayout(.fixed(width: 835, height: 340))
+.previewInterfaceOrientation(.landscapeRight)
+        }
     }
 }
